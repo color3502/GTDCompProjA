1cec8aa90527507f6f874ce56adb6e0ebb35b1f6
color3502 <color350it@gmail.com>
2025-06-08 10:39:44 -0300

    Add Build

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using System.IO;
 
 namespace GTDCompanion;
 
@@ -9,8 +10,27 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        try
+        {
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception ex)
+        {
+            // Salva a stacktrace da exceção fatal
+            try
+            {
+                File.WriteAllText("fatal.log", ex.ToString());
+            }
+            catch
+            {
+                // Silencioso caso não consiga salvar (por permissão, etc)
+            }
+            throw; // (Opcional: remova esta linha se não quiser crashar de vez)
+        }
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
